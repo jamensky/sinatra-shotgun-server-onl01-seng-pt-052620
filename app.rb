57c56af799bb09dbcec877cc3e160001cc93ec14
Jamal Rjoul <jamensky1991@gmail.com>
2020-08-16 19:49:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! Crank!! "
+    "Welcome to your app!!!! Crank!! Crank even more!!!"
   end
 
 end
